e7e1419ef517ec55685d43ad12d7ac61a3f53b14
luminita <lumi.moruz@gmail.com>
2014-12-21 13:18:33 +0100

    Small modifications in the spider to work for all years

--- pythonscraper/pythonscraper/spiders/wikipedia_top_spider.py
+++ pythonscraper/pythonscraper/spiders/wikipedia_top_spider.py
@@ -17,7 +17,7 @@ class WikipediaSpider(Spider):
     # before 2012
 
     start_urls = [
-       "http://sv.wikipedia.org/wiki/Melodifestivalen_2002",
+       # "http://sv.wikipedia.org/wiki/Melodifestivalen_2002",
        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2003",
        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2004",
        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2005",
@@ -27,20 +27,20 @@ class WikipediaSpider(Spider):
        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2009",   
        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2010",   
        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2011",   
+       # "http://sv.wikipedia.org/wiki/Melodifestivalen_2012",   
+         
     ]
-    """
     start_urls = [
-        # "http://sv.wikipedia.org/wiki/Melodifestivalen_2012",   
         #"http://sv.wikipedia.org/wiki/Melodifestivalen_2013",   
         "http://sv.wikipedia.org/wiki/Melodifestivalen_2014",   
    ]
-   """
+
     def _is_start_positions_table(self, table):     
         header_rows = table.css("tr th").extract()
         if len(header_rows) != 5:
             return False
         s = " ".join(table.css("tr th::text").extract())
-        if s.find(u"Upphovsmän") == -1 or s.find(u"Låt") == -1:
+        if s.find(u"Upphovsmän") == -1  or s.find(u"Låt") == -1 or s.find(u"#") == -1 or s.find(u"Del") == -1:
             return False
         return True
     
@@ -93,9 +93,18 @@ class WikipediaSpider(Spider):
         val = object.css(css_selector + "::text").extract()
         if not val:
             val = object.css(css_selector + " i::text").extract()
+        if not val:
+            val = object.css(css_selector + " b::text").extract()
         if val:
             return val[0]
+
         return val
+
+    def _is_year(self, response, years):
+        for year in years:
+            if response.url.split("/")[4].find(year) != -1:
+                return True
+        return False
 
     def parse(self, response):
         entry = items.ResultItem()
@@ -103,31 +112,45 @@ class WikipediaSpider(Spider):
         # start with the table giving the start order
         tables = response.css(".wikitable")
         start_order = self._get_start_order(tables)
-        print start_order
-        
+        print "START ORDER: ", start_order
+
         # now go to the table with the results 
         final_results_table = self._get_final_results_table(tables)
         # get the index of the columns of interest 
-        jury_points_idx = self._get_column_index(final_results_table, [u"jury"])
+        jury_points_idx = self._get_column_index(final_results_table, [u"jury-"])
         tel_points_idx = self._get_column_index(final_results_table, [u"tel", u"poäng"])
         final_place_idx = self._get_column_index(final_results_table, [u"plac"])
         if jury_points_idx == -1 or tel_points_idx == -1 or final_place_idx == -1:
-            print "ERROR: unable to get the index of the columns of interest: ", jury_points_idx, tel_points_idx, final_place_idx
-            return 
+            if self._is_year(response, ["2013"]):
+                jury_points_idx = 14
+                tel_points_idx = 17
+                final_place_idx = 19
+            else:
+                print "ERROR: unable to get the index of the columns of interest: ", jury_points_idx, tel_points_idx, final_place_idx
+                return 
 
         filename = response.url.split("/")[4] + "_result.json"
         f = open(filename, "wb")
         f.write("[\n")
         results = []
-        rows = final_results_table.css("tr")
-        for r in rows[1:]:
+        rows = final_results_table.css("tr")[1:]
+        if self._is_year(response, ["2013"]):
+            rows = rows[1:]
+        for r in rows:
             song =  ''.join(r.css("td:nth-child(2) a::text").extract()).replace('"', "").strip()
+            if not song:
+                song = r.css("td:nth-child(2)::text").extract()[0].replace('"', "").replace(u"”", "").replace(u"”","").strip()
+
             jury_points = self._get_formatted_text("td:nth-child(" + str(jury_points_idx) + ")", r)
             tel_points = self._get_formatted_text("td:nth-child(" + str(tel_points_idx) + ")", r) 
-            final_place = r.css("td:nth-child(" + str(final_place_idx) + ")::text").extract()[0]
+            if self._is_year(response, ["2013", "2014"]):
+                final_place = self._get_formatted_text("td:nth-child(" + str(final_place_idx) + ")", r)
+            else:
+                final_place = r.css("td:nth-child(" + str(final_place_idx) + ")::text").extract()[0]
             
             p = items.ResultItem()
             p['startposition_final'] = -1
+        
             if song in start_order:
                 p['startposition_final'] = start_order[song]
             p["song"] = song
@@ -143,81 +166,3 @@ class WikipediaSpider(Spider):
         f.close()    
      
         return results
-
-        """
-        filename = response.url.split("/")[4] + "_result.json"
-        f = open(filename, "wb")
-        results = []
-        f.write("[\n")
-
-        for t in relevant_tables:
-            rows = t.css("tr")
-            for r in rows[1:]:
-                p = items.ParticipantItem()
-                p["round"] = round_number
-                p["startposition"] = r.css("td:nth-child(1)::text").extract()[0]
-                p["artist"] = " & ".join(r.css("td:nth-child(2) a::text").extract())
-                p["artist_wikilink"] = " & ".join([self.DOMAIN + l for l in r.css("td:nth-child(2) a::attr(href)").extract()])
-                
-                p["song"] = r.css("td:nth-child(3) a::text").extract()
-                if not p["song"]:
-                    p["song"] = " ".join([sp.strip().replace('"', '').replace(u'”', u'') for sp in r.css("td:nth-child(3)::text").extract()])
-                else:
-                    p["song"] = p["song"][0]
-
-                names = [v for v in r.css("td:nth-child(4) a::text").extract() if not v in self.SKIP_LIST]
-                textmusic = [v.strip() for v in r.css("td:nth-child(4)::text").extract() if len(v.strip()) > 0]
-                if len(names) > 0 and not textmusic:
-                    textmusic = ["()"]*len(names)
-                    
-                if len(names) > len(textmusic):
-                    print "WARNING, CHECK NAMES FOR: "
-                    print "Song=", p["song"]
-                    print "Names=", names
-                    print "Text music:", textmusic
-                    for j in range(len(names) - len(textmusic)):
-                        textmusic.append("()")
-
-                p["textmusic"] = " & ".join([names[i] + str(textmusic[i]) for i in range(len(names))])
-                p["textmusic_wikilinks"] = " & ".join([self.DOMAIN + l for l in r.css("td:nth-child(4) a::attr(href)").extract()])
-                
-                votes1 = self._get_formatted_text("td:nth-child(5)", r)
-                votes2 = self._get_formatted_text("td:nth-child(6)", r)
-                if votes1:
-                    p["votes_round1"] = votes1.replace(u'\xa0', u'')
-                else:
-                    p["votes_round1"] = "-"
-
-                if votes2:
-                    p["votes_round2"] = votes2.replace(u'\xa0', u'')
-                else:
-                    p["votes_round2"] = "-"
-    
-
-                place_col_index = "7"
-                remark_col_index = "8"
-                if table_after_2012:
-                    place_col_index = "8"
-                    remark_col_index = "9"
-                
-                p["place"] = r.css("td:nth-child(" + place_col_index + ")::text").extract()[0]
-
-                remark = r.css("td:nth-child("+ remark_col_index +")::text").extract()                
-                if not remark:
-                    remark = r.css("td:nth-child("+ remark_col_index +") a::text").extract()[0]
-                elif type(remark) is list:
-                    remark = remark[0]
-                p["remark"] = remark
-
-                line = json.dumps(dict(p), ensure_ascii=False, indent=4, sort_keys=True) + ",\n"   
-                f.write(line.encode("utf-8"))
-
-                participants.append(p)
-            round_number += 1
-        f.write("\n]")
-        
-        
-        f.close()    
-     
-        return participants
-        """
