728e90cca861a8940754706a6742099635353527
LCHansson <love.hansson@gmail.com>
2015-01-08 20:46:51 +0100

    Script to load data as table into R for further analysis

---
+++ analysis/read_json.R
@@ -0,0 +1,24 @@
+## Libraries ----
+library("data.table")
+library("dplyr")
+library("jsonlite")
+library("textcat")
+
+## Get raw JSON ----
+con <- file.path("./data/all_participants_all_data_2002_2014.json")
+data <- fromJSON(con)
+
+## Convert to data.frame ----
+mello_data <- rbind_all(data)
+
+## Munge ---
+## Language for songs without language in the database (i.e. 2012-2014 songs)
+mello_data <- tbl_dt(mello_data)
+mello_data[
+  language == "" & lyrics != "",
+  language := textcat(lyrics)
+  ]
+mello_data[language == "scots", language := "english"]
+# Inspect results
+mello_data$language %>% table
+
