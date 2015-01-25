6b0e40ae330b201e32a14bc5d16420d02518b156
Cédric Perriard <c@perriard.se>
2015-01-25 01:52:28 +0100

    fixed loading in _LH

---
+++ frontend/js/main_LH.js
@@ -0,0 +1,16 @@
+$(function () {
+    //var firstPostLoaded = false;
+    $('#lasVidareID').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $collapse = $this.closest('.collapse-group').find('.collapse');
+        $collapse.collapse('toggle');
+        var postNr = $this.closest('.collapse-group').attr("data-post")
+        /*if ( !firstPostLoaded &postNr == 1 ){
+            week1Collapse();
+            firstPostLoaded = true;
+        }*/
+    });
+
+    week1();
+});
