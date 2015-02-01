6d60188f668547b56b33bc1cd8c56b662a5f9ed2
Cédric Perriard <c@perriard.se>
2015-02-01 16:13:09 +0100

    implementing fb button

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,16 +1,25 @@
 $(function () {
     var firstPostLoaded = false;
-    $('#lasVidareID').on('click', function(e) {
+    $('.read-on').on('click', function(e) {
         e.preventDefault();
         var $this = $(this);
         var $collapse = $this.closest('.collapse-group').find('.collapse');
         $collapse.collapse('toggle');
         var postNr = $this.closest('.collapse-group').attr("data-post")
-        if ( !firstPostLoaded &postNr == 1 ){
+        if ( !firstPostLoaded & postNr == 1 ){
             week1Collapse();
             firstPostLoaded = true;
         }
     });
 
     week1();
+
+    window.fbShare = function(){
+        FB.ui(
+            {
+                method: 'share',
+                href: 'https://developers.facebook.com/docs/'
+            }, function(response){});
+        return false;
+    }
 });
