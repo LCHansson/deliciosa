de3364e619b6881c290767dd806d4336b2d6b5f4
luminita <lumi.moruz@gmail.com>
2015-02-01 13:57:50 +0100

    Added info about artists and year in the modal

--- frontend/python-scripts/week1-reformat-love-counts-json.py
+++ frontend/python-scripts/week1-reformat-love-counts-json.py
@@ -23,9 +23,13 @@ obj.close()
 # get the lyrics as separate json 
 outfolder = "../data/lyrics/"
 for d in old_data:
-    s = d["song_name"].encode("utf-8")
+    name =  d["artist"].encode("utf-8")
+    name += ": "
+    name += d["song_name"].encode("utf-8")
+    name += " (" + str(d["year"]) + ")"
+
     o = {"lyrics": d["lyrics_cleaned"].encode("utf-8"),
-         "song_name": s[0].upper() + s[1:]}
+         "song_name": name}
     f = os.path.join(outfolder, d["id"] + "_lyrics.json")
     obj = open(f, "w")
     json.dump(o, obj, indent=4, sort_keys=True, encoding="utf-8", ensure_ascii=False)
