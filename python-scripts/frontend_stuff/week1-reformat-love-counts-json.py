851c08a853c082c878fdd58f24d245f1d6fac2a6
luminita <lumi.moruz@gmail.com>
2015-02-04 23:05:58 +0100

    moved python script from frontend

--- python-scripts/frontend_stuff/week1-reformat-love-counts-json.py
+++ python-scripts/frontend_stuff/week1-reformat-love-counts-json.py
@@ -2,6 +2,12 @@ import json
 import os 
 import sys
 
+sent_data = json.load(open("../data/texterna_sent_lovew_counts.json"))
+x = sent_data["data"]
+sd = {}
+for dd in x:
+    sd[dd[3]] = (dd[1], dd[2]) 
+
 old_data = json.load(open("../data/texterna_lovecounts.json"))
 
 # data need to the dataTable in week 1
@@ -29,7 +35,7 @@ for d in old_data:
     name += " (" + str(d["year"]) + ")"
 
     o = {"lyrics": d["lyrics_cleaned"].encode("utf-8"),
-         "song_name": name}
+         "song_name": name, "happy_score": sd[d["id"]][1], "no_love_words": sd[d["id"]][0]}
     f = os.path.join(outfolder, d["id"] + "_lyrics.json")
     obj = open(f, "w")
     json.dump(o, obj, indent=4, sort_keys=True, encoding="utf-8", ensure_ascii=False)
