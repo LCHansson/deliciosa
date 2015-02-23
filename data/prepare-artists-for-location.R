0f7a27f35322cc2ef797f4b5f43bfc34cca374e5
Cédric Perriard <c@perriard.se>
2015-02-23 21:35:14 +0100

    prepare artists for collection

---
+++ data/prepare-artists-for-location.R
@@ -0,0 +1,29 @@
+## Libraries ----
+library("dplyr")
+library("jsonlite")
+library(stringr)
+library(geosphere)
+
+## Get raw JSON ----
+con <- file.path("./data/all_participants_all_data_2002_2014_2.json")
+data <- fromJSON(con)
+
+## Convert to data.frame ----
+mello_data <- as_data_frame(data)
+
+## only keep one row per artist ----
+artists <- mello_data %>% select(id, artist, artist_wikilink) %>% group_by(artist) %>% filter(row_number() == 1)
+
+## remove edit links ----
+artists[grepl("&action=edit&redlink=1", artists$artist_wikilink), ]$artist_wikilink <- ""
+
+## add variables 
+
+artists <- artists %>% mutate(birthplace = "", residence = "", birthyear = "")
+
+
+## export to csv ----
+write.csv(artists, file = "data/artists_links.csv", row.names = FALSE)
+
+
+
