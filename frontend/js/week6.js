d3f3fbfbf38daca4bece09b0d4d2931bf58ee804
Cédric Perriard <c@perriard.se>
2015-03-08 16:34:06 +0100

    spiderweb

--- frontend/js/week6.js
+++ frontend/js/week6.js
@@ -103,20 +103,17 @@ function buildWinnerSpider( data ){
 
         chart: {
             polar: true,
-            type: 'line'
+            type: "line"
         },
         credits: {
             enabled: false
         },
 
         title: {
-            text: 'Budget vs spending',
-            x: -80
+            text: 'Budget vs spending'
         },
 
-        pane: {
-            size: '80%'
-        },
+
 
         xAxis: {
             categories: ['Sales', 'Marketing', 'Development', 'Customer Support',
@@ -137,24 +134,25 @@ function buildWinnerSpider( data ){
         },
 
         legend: {
-            align: 'right',
-            verticalAlign: 'top',
-            y: 70,
-            layout: 'vertical'
+            align: 'left',
+            verticalAlign: 'bottom',
+            layout: 'horizontal'
         },
 
         series: [{
             name: 'Allocated Budget',
-            data: [43000, 19000, 60000, 35000, 17000, 10000],
-            pointPlacement: 'on'
+            data: [43000, 19000, 60000, 35000, 17000, 10000]
         }, {
             name: 'Actual Spending',
-            data: [50000, 39000, 42000, 31000, 26000, 14000],
-            pointPlacement: 'on'
+            data: [50000, 39000, 42000, 31000, 26000, 14000]
         }]
 
     });
+
+
 }
+
+
 
 function week6Collapse(){
     buildWinnerSpider();
